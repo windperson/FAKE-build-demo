b611aebc09e08a7ed96427c2a00135f6f730e569
windperson <windperson@gmail.com>
2016-06-13 17:51:34 +0800

    run NUnit unit test in serial sequence

--- build.fsx
+++ build.fsx
@@ -1,29 +1,48 @@
 // include Fake lib
 #r @"packages\FAKE\tools\FakeLib.dll"
-open Fake
+open Fake;
+
+RestorePackages()
 
 // Properties
-let buildDir = "./build/"
+let buildDir = "./build/";
+let testDir = "./test/";
 
 // Targets
 Target "Clean" (fun _ ->
     CleanDir buildDir
-)
+);
 
 Target "BuildApp" (fun _ ->
     !! "src/app/**/*.csproj"
         |> MSBuildRelease buildDir "Build"
         |> Log "AppBuild-Output: "
+);
+
+Target "BuildTest" (fun _ ->
+    !! "src/test/**/*.csproj"
+        |> MSBuildDebug testDir "Build"
+        |> Log "TestBuild-Output: "
+);
+
+Target "Test" (fun _ ->
+    !! (testDir + "/NUnit.Test.*.dll")
+        |> NUnit (fun p ->
+        {p with
+            DisableShadowCopy = true;
+            OutputFile = testDir + "TestResults.xml" })
 )
 
 // Default target
 Target "Default" (fun _ ->
     trace "Hello World from FAKE"
-)
+);
 
 // Dependencies
 "Clean"
     ==> "BuildApp"
+    ==> "BuildTest"
+    ==> "Test"
     ==> "Default"
 
 // start build
