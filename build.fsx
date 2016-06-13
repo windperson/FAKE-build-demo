7ef60ccbd9b10d28cbe4a6f7470566dc315951e9
windperson <windperson@gmail.com>
2016-06-13 15:13:28 +0800

    add real build c sharp project target

--- build.fsx
+++ build.fsx
@@ -2,10 +2,29 @@
 #r @"packages\FAKE\tools\FakeLib.dll"
 open Fake
 
+// Properties
+let buildDir = "./build/"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "BuildApp" (fun _ ->
+    !! "src/app/**/*.csproj"
+        |> MSBuildRelease buildDir "Build"
+        |> Log "AppBuild-Output: "
+)
+
 // Default target
 Target "Default" (fun _ ->
     trace "Hello World from FAKE"
 )
+
+// Dependencies
+"Clean"
+    ==> "BuildApp"
+    ==> "Default"
 
 // start build
 RunTargetOrDefault "Default"
